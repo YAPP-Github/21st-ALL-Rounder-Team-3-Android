9a5a93c9920ca0ad68396deb7d2b83aebf6e28f2
EvergreenTree97 <tkdfhr7497@naver.com>
2022-12-04 21:40:06 +0900

    feat: 메인 변경사항 추가

--- presentation/src/main/java/com/yapp/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.yapp.presentation.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.yapp.presentation.theme.AllRounder3Theme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,7 +11,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            AllRounder3Theme {
+                MainScreen()
+            }
         }
     }
 }
