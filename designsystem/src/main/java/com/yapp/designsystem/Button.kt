566bc20aaf4ba7a9949ad0bf4f36ed9cbaef4397
EvergreenTree97 <tkdfhr7497@naver.com>
2022-12-11 20:19:11 +0900

    feat: DesignSystem 모듈 생성

---
+++ designsystem/src/main/java/com/yapp/designsystem/Button.kt
@@ -0,0 +1,11 @@
+package com.yapp.designsystem
+
+import androidx.compose.material.Button
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TimiButton(){
+    Button(onClick = { /*TODO*/ }) {
+
+    }
+}
