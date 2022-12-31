5a8eccd8b0bbceaf137252c2dae0321fd7c2cf2c
jsh-me <ppm_it@naver.com>
2023-01-01 00:54:43 +0900

    feat: 필요한 network dependency 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -5,11 +5,24 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.dagger.hilt)
     alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
     compileSdk = libs.versions.compile.sdk.get().toInt()
     namespace = "com.yapp.data"
+
+    buildTypes {
+        debug {
+            buildConfigField("String", "BASE_URL", "\"https://timitimi.site\"")
+        }
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            buildConfigField("String", "BASE_URL", "\"https://timitimi.site\"")
+        }
+    }
+
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.majorVersion
     }
@@ -22,4 +35,8 @@ dependencies {
     implementation(libs.hilt.android)
     kapt(libs.hilt.kapt)
     implementation(libs.timber)
+
+    implementation(libs.bundles.network)
+    debugImplementation(libs.chuncker.library)
+    releaseImplementation(libs.chuncker.library.no.op)
 }
