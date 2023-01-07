d372b18151e275c6203a93459d4554d836c7f81b
Choi Sang Rok <tkdfhr7497@naver.com>
2023-01-07 18:36:16 +0900

    taskcard, badge 재 구현 (#17)
    * Half Roundcorner 뱃지 구현
    
    * TaskCard 재구현

--- designsystem/src/main/java/com/yapp/designsystem/component/TaskCard.kt
+++ designsystem/src/main/java/com/yapp/designsystem/component/TaskCard.kt
@@ -2,23 +2,33 @@ package com.yapp.designsystem.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,11 +36,8 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.yapp.designsystem.R
 import com.yapp.designsystem.border.TimiBorder
-import com.yapp.designsystem.modifier.timiClipBorder
 import com.yapp.designsystem.row.CenterVerticallyRow
-import com.yapp.designsystem.theme.Gray200
 import com.yapp.designsystem.theme.Gray300
-import com.yapp.designsystem.theme.Gray500
 import com.yapp.designsystem.theme.Gray600
 import com.yapp.designsystem.theme.Gray700
 import com.yapp.designsystem.theme.Green100
@@ -44,176 +51,190 @@ fun TimiTaskCard(
     profile: String,
     name: String,
     period: String,
+    badgeText: String,
     content: String,
     subContent: String,
-    progress: Float,
-    progressBarText: String,
     taskType: TaskType,
+    isMe: Boolean,
 ) {
+    val highLightColor = when (taskType) {
+        is TaskType.Request -> Purple500
+        else -> Color.White
+    }
     val (mainColor, backgroundColor, borderColor) = when (taskType) {
-        TaskType.Progress -> {
+        is TaskType.Request -> {
             listOf(Purple500, Purple100.copy(alpha = 0.5f), Purple100)
         }
-        is TaskType.Request -> {
+
+        TaskType.Progress -> {
             listOf(Green500, Green100.copy(alpha = 0.5f), Green100)
         }
-        is TaskType.Done -> {
-            listOf(Gray500, Gray300.copy(alpha = 0.5f), Gray300)
+
+        else -> {
+            listOf(Gray600, Gray300.copy(alpha = 0.5f), Gray300)
         }
     }
-    Column(
-        modifier = modifier
-            .timiClipBorder(
-                border = TimiBorder(color = Gray200),
-                shape = RoundedCornerShape(16.dp)
+    val density = LocalDensity.current
+    var boxHeight by remember { mutableStateOf(0.dp) }
+
+    Row(
+        modifier = Modifier.clip(
+            shape = RoundedCornerShape(
+                topStart = 16.dp,
+                bottomStart = 16.dp,
             )
-            .background(Color.White)
-            .padding(top = 14.dp)
-            .padding(horizontal = 16.dp),
+        )
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+        Box(
+            modifier = Modifier
+                .width(8.dp)
+                .height(boxHeight + 32.dp)
+                .background(highLightColor)
+        )
+        Column(
+            modifier = modifier
+                .clip(
+                    shape = RoundedCornerShape(
+                        topEnd = 16.dp,
+                        bottomEnd = 16.dp,
+                    )
+                )
+                .background(Color.White)
+                .padding(vertical = 16.dp)
+                .padding(start = 16.dp, end = 20.dp)
+                .onSizeChanged {
+                    with(density) {
+                        boxHeight = it.height.toDp()
+                    }
+                },
         ) {
-            TaskContent(
-                profile = profile,
-                name = name,
-                period = period,
-                content = content,
-                subContent = subContent,
-                mainColor = mainColor,
-                backgroundColor = backgroundColor,
-                borderColor = borderColor,
-            )
-            TimiCircularProgressBar(
-                centerText = progressBarText,
-                progress = progress,
-                color = mainColor,
-                backgroundColor = backgroundColor,
-            )
-        }
-        when (taskType) {
-            TaskType.Progress -> {
-                Spacer(modifier = Modifier.padding(top = 14.dp))
-            }
-            is TaskType.Request -> {
-                Divider(
-                    modifier = Modifier.padding(
-                        top = 12.dp,
-                        bottom = 8.dp
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                TimiBody3Regular(
+                    text = period,
+                    color = Gray600,
+                )
+                TimiHalfRoundedBadge(
+                    text = badgeText,
+                    border = TimiBorder(
+                        color = borderColor
                     ),
+                    backgroundColor = backgroundColor,
+                    fontColor = mainColor,
+                )
+            }
+            TimiH3SemiBold(
+                modifier = Modifier.padding(top = 8.dp),
+                text = content,
+                singleLine = true,
+            )
+            TimiBody3Regular(
+                text = subContent,
+                color = Gray600,
+                singleLine = true,
+            )
+            if (isMe.not()) {
+                Divider(
+                    modifier = Modifier.padding(vertical = 10.dp),
                     color = Gray300
                 )
-                BottomRequestArea(
-                    text = stringResource(
-                        id = R.string.remain_check_period,
-                        taskType.confirmationPeriod
-                    ),
-                    color = mainColor
+                BottomArea(
+                    profile = profile,
+                    name = name,
+                    isMe = isMe,
                 ) {
-                    CurrentTask(
-                        currentConfirmation = taskType.currentConfirmation,
-                        totalConfirmation = taskType.totalConfirmation,
-                        color = mainColor,
-                    )
+                    when (taskType) {
+                        is TaskType.Request -> {
+                            CurrentTask(
+                                currentConfirmation = taskType.currentConfirmation,
+                                totalConfirmation = taskType.totalConfirmation,
+                            )
+                        }
+
+                        is TaskType.Done -> {
+                            DoneTask(
+                                completedTask = taskType.completedTask,
+                                notYetTask = taskType.notYetTask,
+                                color = mainColor,
+                            )
+                        }
+
+                        else -> {}
+                    }
                 }
-            }
-            is TaskType.Done -> {
-                Divider(
-                    modifier = Modifier.padding(
-                        top = 12.dp,
-                        bottom = 8.dp
-                    ),
-                    color = Gray300
-                )
-                BottomRequestArea(
-                    text = stringResource(id = R.string.complete),
-                    color = mainColor
-                ) {
-                    DoneTask(
-                        completedTask = taskType.completedTask,
-                        notYetTask = taskType.notYetTask,
-                        color = mainColor,
-                    )
+            } else {
+                when (taskType) {
+                    is TaskType.Request -> {
+                        Divider(
+                            modifier = Modifier.padding(vertical = 10.dp),
+                            color = Gray300
+                        )
+                        BottomArea(
+                            profile = profile,
+                            name = name,
+                            isMe = isMe,
+                        ) {
+                            CurrentTask(
+                                currentConfirmation = taskType.currentConfirmation,
+                                totalConfirmation = taskType.totalConfirmation,
+                            )
+                        }
+                    }
+
+                    is TaskType.Done -> {
+                        Divider(
+                            modifier = Modifier.padding(vertical = 10.dp),
+                            color = Gray300
+                        )
+                        BottomArea(
+                            profile = profile,
+                            name = name,
+                            isMe = isMe,
+                        ) {
+                            DoneTask(
+                                completedTask = taskType.completedTask,
+                                notYetTask = taskType.notYetTask,
+                                color = mainColor,
+                            )
+                        }
+                    }
+
+                    else -> {}
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun TaskContent(
+fun BottomArea(
+    modifier: Modifier = Modifier,
     profile: String,
     name: String,
-    period: String,
-    content: String,
-    subContent: String,
-    mainColor: Color,
-    backgroundColor: Color,
-    borderColor: Color,
+    isMe: Boolean,
+    rightComponent: (@Composable () -> Unit)? = null,
 ) {
-    Column {
-        Row(
-            modifier = Modifier
-                .padding(bottom = 10.dp),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            AsyncImage(
-                modifier = Modifier
-                    .size(size = 26.dp)
-                    .clip(CircleShape),
-                model = profile,
-                contentScale = ContentScale.Crop,
-                contentDescription = "Task Profile"
-            )
-            TimiCaption2SemiBold(
-                modifier = Modifier.padding(
-                    start = 6.dp,
-                    end = 12.dp
-                ),
-                text = name,
-                color = Gray700
-            )
-            TimiSmallRoundedBadge(
-                text = period,
-                border = TimiBorder(
-                    color = borderColor
-                ),
-                backgroundColor = backgroundColor,
-                fontColor = mainColor,
-            )
-        }
-        TimiH4SemiBold(
-            modifier = Modifier.padding(bottom = 2.dp),
-            text = content
-        )
-        TimiBody4Regular(
-            text = subContent,
-            color = Gray600,
-        )
+    val arrangement = if(isMe){
+        Arrangement.End
+    }else{
+        Arrangement.SpaceBetween
     }
-}
-
-@Composable
-fun BottomRequestArea(
-    modifier: Modifier = Modifier,
-    text: String,
-    color: Color,
-    rightComponent: @Composable () -> Unit,
-) {
     Row(
         modifier = modifier
-            .fillMaxWidth()
-            .padding(bottom = 11.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
+            .fillMaxWidth(),
+        horizontalArrangement = arrangement,
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        TimiCaption1SemiBold(
-            text = text,
-            color = color
-        )
-        rightComponent()
+        if (isMe.not()) {
+            ProfileRow(
+                profile = profile,
+                name = name,
+            )
+        }
+        rightComponent?.invoke()
     }
 }
 
@@ -221,13 +242,44 @@ fun BottomRequestArea(
 private fun CurrentTask(
     currentConfirmation: Int,
     totalConfirmation: Int,
-    color: Color,
 ) {
     CenterVerticallyRow(horizontalSpace = 6.dp) {
-        TimiCaption2SemiBold(text = stringResource(id = R.string.confirmation_status))
+        TimiCaption2SemiBold(
+            text = stringResource(id = R.string.confirmation_status),
+            color = Gray600,
+        )
         TimiCaption2SemiBold(
             text = "$currentConfirmation/$totalConfirmation",
-            color = color,
+            color = Gray600,
+        )
+    }
+}
+
+
+@Composable
+private fun ProfileRow(
+    profile: String,
+    name: String,
+) {
+    Row(
+        modifier = Modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .size(size = 20.dp)
+                .clip(CircleShape),
+            model = profile,
+            contentScale = ContentScale.Crop,
+            contentDescription = "Task Profile"
+        )
+        TimiCaption2SemiBold(
+            modifier = Modifier.padding(
+                start = 6.dp,
+                end = 12.dp
+            ),
+            text = name,
+            color = Gray700
         )
     }
 }
@@ -264,7 +316,9 @@ private fun DoneTask(
     }
 }
 
+
 sealed class TaskType {
+    object NotStarted : TaskType()
     object Progress : TaskType()
     data class Request(
         val confirmationPeriod: Int,
@@ -282,8 +336,11 @@ sealed class TaskType {
 @Composable
 fun CardPreview() {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(4.dp)
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
         TimiTaskCard(
             profile = "https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80",
@@ -291,9 +348,19 @@ fun CardPreview() {
             period = "12.2 ~ 12.7",
             content = "참고문헌 검토하기",
             subContent = "표기법 준수할 것",
-            progress = 0.9f,
-            progressBarText = "D-13",
+            badgeText = "D-13",
+            taskType = TaskType.NotStarted,
+            isMe = true,
+        )
+        TimiTaskCard(
+            profile = "https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80",
+            name = "가연",
+            period = "12.2 ~ 12.7",
+            content = "참고문헌 검토하기",
+            subContent = "표기법 준수할 것",
+            badgeText = "D-13",
             taskType = TaskType.Progress,
+            isMe = true,
         )
 
         TimiTaskCard(
@@ -302,13 +369,13 @@ fun CardPreview() {
             period = "12.2 ~ 12.7",
             content = "참고문헌 검토하기",
             subContent = "표기법 준수할 것",
-            progress = 0.9f,
-            progressBarText = "D-13",
+            badgeText = "D-13",
             taskType = TaskType.Request(
                 confirmationPeriod = 3,
                 currentConfirmation = 5,
                 totalConfirmation = 6,
             ),
+            isMe = true,
         )
         TimiTaskCard(
             profile = "https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80",
@@ -316,12 +383,60 @@ fun CardPreview() {
             period = "12.2 ~ 12.7",
             content = "참고문헌 검토하기",
             subContent = "표기법 준수할 것",
-            progress = 0.9f,
-            progressBarText = "D-13",
+            badgeText = "D-13",
             taskType = TaskType.Done(
                 completedTask = 4,
                 notYetTask = 2,
             ),
+            isMe = true,
+        )
+        TimiTaskCard(
+            profile = "https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80",
+            name = "가연",
+            period = "12.2 ~ 12.7",
+            content = "참고문헌 검토하기",
+            subContent = "표기법 준수할 것",
+            badgeText = "D-13",
+            taskType = TaskType.NotStarted,
+            isMe = false,
+        )
+        TimiTaskCard(
+            profile = "https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80",
+            name = "가연",
+            period = "12.2 ~ 12.7",
+            content = "참고문헌 검토하기",
+            subContent = "표기법 준수할 것",
+            badgeText = "D-13",
+            taskType = TaskType.Progress,
+            isMe = false,
+        )
+
+        TimiTaskCard(
+            profile = "https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80",
+            name = "가연",
+            period = "12.2 ~ 12.7",
+            content = "참고문헌 검토하기",
+            subContent = "표기법 준수할 것",
+            badgeText = "D-13",
+            taskType = TaskType.Request(
+                confirmationPeriod = 3,
+                currentConfirmation = 5,
+                totalConfirmation = 6,
+            ),
+            isMe = false,
+        )
+        TimiTaskCard(
+            profile = "https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80",
+            name = "가연",
+            period = "12.2 ~ 12.7",
+            content = "참고문헌 검토하기sdsdsdsdsdsdsdsdsdsdsdsds",
+            subContent = "표기법 준수할 것sdsdsddsdsdsddsdsdsdssdsdsdsddsd",
+            badgeText = "D-13",
+            taskType = TaskType.Done(
+                completedTask = 4,
+                notYetTask = 2,
+            ),
+            isMe = false,
         )
     }
 }
