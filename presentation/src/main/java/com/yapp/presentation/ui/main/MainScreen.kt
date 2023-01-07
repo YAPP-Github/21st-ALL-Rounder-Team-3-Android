55ac2d29b4dc077de238d043149fae1160897f91
EvergreenTree97 <tkdfhr7497@naver.com>
2023-01-07 21:29:03 +0900

    main 구현

--- presentation/src/main/java/com/yapp/presentation/ui/main/MainScreen.kt
+++ presentation/src/main/java/com/yapp/presentation/ui/main/MainScreen.kt
@@ -4,13 +4,8 @@ package com.yapp.presentation.ui.main
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -25,22 +20,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
@@ -53,13 +39,24 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import com.germainkevin.collapsingtopbar.CollapsingTopBarScrollBehavior
 import com.germainkevin.collapsingtopbar.rememberCollapsingTopBarScrollBehavior
+import com.yapp.designsystem.border.TimiBorder
+import com.yapp.designsystem.component.TimiBody2Medium
+import com.yapp.designsystem.component.TimiBody3Regular
+import com.yapp.designsystem.component.TimiCaption1Regular
+import com.yapp.designsystem.component.TimiCaption2Regular
 import com.yapp.designsystem.component.TimiH2SemiBold
 import com.yapp.designsystem.component.TimiH3SemiBold
+import com.yapp.designsystem.component.TimiMediumRoundedBadge
 import com.yapp.designsystem.component.TopBarNotificationIcon
 import com.yapp.designsystem.modifier.drawColoredShadow
+import com.yapp.designsystem.modifier.timiClipBorder
+import com.yapp.designsystem.theme.Gray700
+import com.yapp.designsystem.theme.Purple500
 import com.yapp.presentation.R
-import com.yapp.presentation.ui.main.components.Tasks
+import com.yapp.presentation.ui.main.components.TaskContent
 import com.yapp.presentation.ui.main.redux.MainSingleEvent
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
@@ -68,7 +65,7 @@ fun MainScreen(
     viewModel: MainViewModel = hiltViewModel(),
     onBackPressed: () -> Unit,
 ) {
-    val state = viewModel.viewState.collectAsStateWithLifecycle()
+    val state by viewModel.viewState.collectAsStateWithLifecycle()
 
     LaunchedEffect(viewModel.singleEventFlow) {
         viewModel.singleEventFlow
@@ -104,21 +101,37 @@ fun MainScreen(
 
     val scrollBehavior = rememberCollapsingTopBarScrollBehavior()
     Column(modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)) {
-        Header(scrollBehavior = scrollBehavior)
-        Box(modifier = Modifier.fillMaxSize()) {
-            Tasks(title = "나의 할일")
+        Header(
+            scrollBehavior = scrollBehavior,
+            title = "고전문학사 팀플 3조",
+            memo = "학기 성적 A+ 도전 도전 도전 도전🎃",
+            startDate = "11.16",
+            endDate = "12.7",
+            notificationCount = 2,
+            members = dummyMembers.toImmutableList(),
+        )
+        Box(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            TaskContent(
+                title = "나의 할일"
+            )
         }
     }
 }
 
 @Composable
 fun Header(
-    scrollBehavior: CollapsingTopBarScrollBehavior
+    scrollBehavior: CollapsingTopBarScrollBehavior,
+    title: String,
+    memo: String,
+    startDate: String,
+    endDate: String,
+    notificationCount: Int,
+    members: ImmutableList<Member>,
 ) {
     Card(
-        modifier = Modifier
-            .drawColoredShadow()
-            .animateContentSize(),
+        modifier = Modifier.drawColoredShadow(),
     ) {
         Column(
             Modifier.padding(horizontal = 16.dp),
@@ -126,47 +139,39 @@ fun Header(
             MainTopAppBar(
                 onClickLeftArrow = {},
                 onClickNotification = {},
-                hasNotification = true,
-                centerContent = {
+                leftContent = {
                     TimiH3SemiBold(
-                        text = "2022-2 4차 산업 혁플",
+                        text = title,
                         color = Color.Black
                     )
                 },
                 collapsed = scrollBehavior.isCollapsed,
+                notificationCount = notificationCount,
             )
             AnimatedVisibility(
                 visible = scrollBehavior.isExpanded,
-                enter = fadeIn(),
-                exit = fadeOut(),
             ) {
                 Column {
                     TimiH2SemiBold(
-                        text = "2022-2 4차 산업 혁플",
+                        text = title,
                         color = Color.Black
                     )
-                    Spacer(modifier = Modifier.height(20.dp))
-                    BadgeString(
-                        title = "학기 성적 A+ 도전",
-                        badgeText = "여유 있게",
-                        borderColor = MaterialTheme.colors.primary,
-                        badgeColor = Color.White,
-                        badgeFontColor = MaterialTheme.colors.primary,
-                        space = 11.dp
-                    )
                     Spacer(modifier = Modifier.height(12.dp))
-                    TaskBar(
+                    BadgeString( //TODO(EvergreenTree97) 디데이와 날짜 계산하도록 변경
+                        title = "$startDate ~ $endDate",
                         badgeText = "D-10",
-                        title = "11/16 ~ 12/7"
+                        space = 8.dp
                     )
-                    Spacer(modifier = Modifier.height(14.dp))
+                    Spacer(modifier = Modifier.height(10.dp))
+                    TimiBody3Regular(text = memo)
                 }
             }
+            Spacer(modifier = Modifier.height(12.dp))
             MemberContents(
                 title = "팀원 7명",
-                memebers = dummyMembers
+                members = members
             )
-            Spacer(modifier = Modifier.height(18.dp))
+            Spacer(modifier = Modifier.height(20.dp))
         }
     }
 }
@@ -177,7 +182,7 @@ fun MainTopAppBar(
     onClickLeftArrow: () -> Unit,
     onClickNotification: () -> Unit,
     notificationCount: Int,
-    centerContent: @Composable () -> Unit = {},
+    leftContent: @Composable () -> Unit = {},
     collapsed: Boolean,
 ) {
     Box(
@@ -192,59 +197,35 @@ fun MainTopAppBar(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            if (collapsed) {
-                Row(horizontalArrangement = Arrangement.spacedBy(space = 16.dp)) {
-                    Image(
-                        modifier = Modifier.clickable(
-                            onClick = onClickLeftArrow
-                        ),
-                        painter = painterResource(id = com.yapp.designsystem.R.drawable.icon_arrow_left),
-                        contentDescription = "leftArrow"
-                    )
-                    centerContent()
+            Image(
+                modifier = Modifier.clickable(
+                    onClick = onClickLeftArrow
+                ),
+                painter = painterResource(id = com.yapp.designsystem.R.drawable.icon_arrow_left),
+                contentDescription = "leftArrow"
+            )
+            Row(
+                modifier = Modifier.weight(1f),
+                horizontalArrangement = if (collapsed) {
+                    Arrangement.Start
+                } else {
+                    Arrangement.Center
                 }
-            } else {
-                Image(
-                    modifier = Modifier.clickable(
-                        onClick = onClickLeftArrow
-                    ),
-                    painter = painterResource(id = com.yapp.designsystem.R.drawable.icon_arrow_left),
-                    contentDescription = "leftArrow"
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.logo),
-                    contentDescription = "logo"
-                )
+            ) {
+                if (collapsed) {
+                    leftContent()
+                } else {
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = "logo"
+                    )
+                }
             }
             TopBarNotificationIcon(
                 count = notificationCount,
                 onClick = onClickNotification,
             )
         }
-    }
-}
-
-
-@Composable
-fun TaskBar(
-    title: String,
-    badgeText: String,
-) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(space = 20.dp),
-    ) {
-        BadgeString(
-            title = title,
-            badgeText = badgeText,
-            borderColor = Color.White,
-            badgeColor = MaterialTheme.colors.primary,
-            badgeFontColor = Color.White,
-            space = 11.dp
-        )
-        LinearProgressBar(
-            percent = 0.45f
-        )
     }
 }
 
@@ -263,17 +244,14 @@ val dummyMembers = listOf(
 @Composable
 fun MemberContents(
     title: String,
-    memebers: List<Member>
+    members: ImmutableList<Member>
 ) {
     Column(
         verticalArrangement = Arrangement.spacedBy(space = 6.dp)
     ) {
-        Text(
-            text = title,
-            color = Color.Black
-        )
+        TimiCaption1Regular(text = title)
         LazyRow(
-            horizontalArrangement = Arrangement.spacedBy(12.dp)
+            horizontalArrangement = Arrangement.spacedBy(13.dp)
         ) {
             item {
                 MemberProfile(
@@ -283,13 +261,13 @@ fun MemberContents(
                 )
             }
             items(
-                items = memebers,
-                key = { it.name }
-            ) { memeber ->
+                items = members,
+                key = { it.name },
+            ) { member ->
                 MemberProfile(
-                    profile = memeber.profile,
-                    name = memeber.name,
-                    selected = memeber.selected
+                    profile = member.profile,
+                    name = member.name,
+                    selected = member.selected
                 )
             }
         }
@@ -316,6 +294,15 @@ fun MemberProfile(
             MaterialTheme.colors.primary
         }
     )
+    val border = if (selected) {
+        TimiBorder(
+            width = 2.dp,
+            color = profileBorderColor
+        )
+    } else {
+        null
+    }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(5.dp)
@@ -323,17 +310,15 @@ fun MemberProfile(
         AsyncImage(
             modifier = Modifier
                 .size(size = 40.dp)
-                .clip(CircleShape)
-                .border(
-                    width = 2.dp,
-                    color = profileBorderColor,
-                    shape = CircleShape
+                .timiClipBorder(
+                    border = border,
+                    shape = CircleShape,
                 ),
             model = profile,
             contentScale = ContentScale.Crop,
             contentDescription = null
         )
-        Text(
+        TimiCaption2Regular(
             text = name,
             color = fontColor
         )
@@ -342,124 +327,26 @@ fun MemberProfile(
 
 }
 
-@Composable
-fun LinearProgressBar(
-    percent: Float,
-) {
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(space = 8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = (percent * 100).toInt().toString() + "%",
-            style = MaterialTheme.typography.body1,
-            color = Color.Black,
-        )
-        LinearProgressIndicator(
-            modifier = Modifier.clip(shape = RoundedCornerShape(5.dp)),
-            backgroundColor = MaterialTheme.colors.secondary,
-            color = MaterialTheme.colors.primary,
-            progress = percent,
-        )
-    }
-
-}
 
 @Composable
 fun BadgeString(
     title: String,
     badgeText: String,
-    borderColor: Color,
-    badgeColor: Color,
-    badgeFontColor: Color,
     space: Dp = 0.dp,
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(space = space)
     ) {
-        Badge(
+        TimiMediumRoundedBadge(
             text = badgeText,
-            borderColor = borderColor,
-            backgroundColor = badgeColor,
-            fontColor = badgeFontColor,
+            border = null,
+            backgroundColor = Purple500,
+            fontColor = Color.White
         )
-        Text(
+        TimiBody2Medium(
             text = title,
-            style = MaterialTheme.typography.body2,
-            color = Color.Black
+            color = Gray700
         )
     }
-}
-
-@Composable
-fun Badge(
-    modifier: Modifier = Modifier,
-    text: String,
-    borderColor: Color,
-    backgroundColor: Color,
-    fontColor: Color,
-) {
-    Box(
-        modifier = modifier
-            .border(
-                width = 1.dp,
-                color = borderColor,
-                shape = RoundedCornerShape(8.dp),
-            )
-            .clip(
-                shape = RoundedCornerShape(8.dp),
-            )
-            .background(
-                color = backgroundColor
-            )
-    ) {
-        Text(
-            modifier = Modifier.padding(
-                vertical = 2.dp,
-                horizontal = 8.dp
-            ),
-            text = text,
-            color = fontColor,
-        )
-    }
-}
-
-@Composable
-fun Notification(
-    hasNotification: Boolean,
-    onClick: () -> Unit,
-) {
-    Row(
-        modifier = Modifier.clickable(
-            onClick = onClick,
-        )
-    ) {
-        Image(
-            painter = painterResource(id = com.yapp.designsystem.R.drawable.icon_notification),
-            contentDescription = "bell",
-        )
-        if (hasNotification) {
-            DrawDot(
-                dotSize = 5.dp,
-                color = MaterialTheme.colors.primary,
-            )
-        }
-    }
-}
-
-@Composable
-fun DrawDot(
-    dotSize: Dp,
-    color: Color,
-) {
-    Canvas(
-        modifier = Modifier.size(size = dotSize),
-        onDraw = {
-            val size = dotSize.toPx()
-            drawCircle(
-                color = color, radius = size / 2f
-            )
-        }
-    )
 }
