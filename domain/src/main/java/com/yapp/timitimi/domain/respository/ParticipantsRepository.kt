be4c53b1e2b7958ff1dc3904b3677f8d23c3150c
EvergreenTree97 <tkdfhr7497@naver.com>
2023-01-14 02:10:49 +0900

    domain 작성

--- domain/src/main/java/com/yapp/timitimi/domain/respository/ParticipantsRepository.kt
+++ domain/src/main/java/com/yapp/timitimi/domain/respository/ParticipantsRepository.kt
@@ -1,5 +1,7 @@
 package com.yapp.timitimi.domain.respository
 
+import com.yapp.timitimi.domain.entity.Participant
+
 interface ParticipantsRepository {
     /**
      * 참여자 추가
@@ -7,9 +9,9 @@ interface ParticipantsRepository {
     suspend fun postParticipants(projectId: String)
 
     /**
-     * 참여자 정보 리스트 조회
+     * 참여자 목록 조회
      */
-    suspend fun getProjectParticipants(projectId: String)
+    suspend fun getProjectParticipants(projectId: String): Result<List<Participant>>
 
     /**
      * 참여자 정보 조회
