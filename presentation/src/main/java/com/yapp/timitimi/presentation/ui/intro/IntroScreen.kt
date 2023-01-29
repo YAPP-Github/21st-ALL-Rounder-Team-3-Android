e9d41c4a85eb50d1e0fd7da279903c3724aeb7a5
jsh-me <ppm_it@naver.com>
2023-01-29 20:10:22 +0900

    로그인 화면에 로고 및 백그라운드 수정

--- presentation/src/main/java/com/yapp/timitimi/presentation/ui/intro/IntroScreen.kt
+++ presentation/src/main/java/com/yapp/timitimi/presentation/ui/intro/IntroScreen.kt
@@ -2,52 +2,89 @@ package com.yapp.timitimi.presentation.ui.intro
 
 import android.app.Activity
 import android.content.Context
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import com.yapp.timitimi.component.TimiBody2Medium
 import com.yapp.timitimi.presentation.R
+import com.yapp.timitimi.presentation.ui.createproject.screen.Spacing
 import com.yapp.timitimi.presentation.ui.login.LoginActivity
-import com.yapp.timitimi.presentation.ui.login.LoginViewModel
+import com.yapp.timitimi.theme.Purple200
+import com.yapp.timitimi.theme.Purple500
 import com.yapp.timitimi.ui.startActivityWithAnimation
 
 @Composable
 fun IntroScreen(
-    viewModel: LoginViewModel = hiltViewModel(),
     context: Context = LocalContext.current
 ) {
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = MaterialTheme.colors.primary),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .background(
+                brush = Brush.verticalGradient(listOf(White, Purple200))
+            ),
+        contentAlignment = Alignment.Center
+
     ) {
-        Icon(
-            painter = painterResource(id = R.drawable.kakako_login_button),
+        Column(
             modifier = Modifier
-                .wrapContentSize()
-                .padding(16.dp)
-                .clickable {
-                    (context as Activity).startActivityWithAnimation<LoginActivity>()
-                },
-            contentDescription = "kakao login button",
-            tint = Color.Unspecified,
-        )
+                .wrapContentSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.timitimi),
+                modifier = Modifier
+                    .padding(32.dp),
+                contentDescription = null,
+            )
+
+            TimiBody2Medium(
+                text = stringResource(id = R.string.onboarding_text),
+                color = Purple500,
+                textAlign = TextAlign.Center
+            )
+
+            Spacing(32.dp)
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 80.dp),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.kakako_login_button),
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(16.dp)
+                    .clickable {
+                        (context as Activity).startActivityWithAnimation<LoginActivity>()
+                    },
+                contentDescription = "kakao login button",
+                tint = Color.Unspecified,
+            )
+        }
     }
 }
 
