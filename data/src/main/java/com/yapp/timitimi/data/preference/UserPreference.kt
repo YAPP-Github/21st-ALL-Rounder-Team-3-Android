407d29b986b0dc46ad6b859a74c84b6549d73d98
EvergreenTree97 <tkdfhr7497@naver.com>
2023-02-05 17:01:56 +0900

    첫 프로젝트 여부를 가지는 boolean값 저장

--- data/src/main/java/com/yapp/timitimi/data/preference/UserPreference.kt
+++ data/src/main/java/com/yapp/timitimi/data/preference/UserPreference.kt
@@ -1,8 +1,13 @@
 package com.yapp.timitimi.data.preference
 
 import android.content.Context
+import androidx.core.content.edit
 import com.yapp.timitimi.domain.preference.UserPreference
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class UserPreferenceImpl @Inject constructor(
@@ -35,9 +40,20 @@ class UserPreferenceImpl @Inject constructor(
         defaultValue = ""
     )
 
+    override fun getIsFirstProject(): Flow<Boolean> =
+        flow { prefs.getBoolean(IS_FIRST_PROJECT, true) }
+
+    override suspend fun setIsFirstProject(isFirstProject: Boolean) = withContext(Dispatchers.IO) {
+        prefs.edit {
+            putBoolean(IS_FIRST_PROJECT, isFirstProject)
+        }
+    }
+
+
     companion object {
         private const val PREF_NAME = "token.pref"
         private const val ACCESS_TOKEN = "accessToken"
         private const val REFRESH_TOKEN = "refreshToken"
+        private const val IS_FIRST_PROJECT = "isFirstProject"
     }
 }
