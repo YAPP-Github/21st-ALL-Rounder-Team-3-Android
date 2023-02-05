b6814cc6dee183254358db703f2a1fa9b6e1468e
jsh-me <ppm_it@naver.com>
2023-02-06 01:52:57 +0900

    add jetpack library

---
+++ core/src/main/java/com/yapp/timitimi/ui/BaseActivity.kt
@@ -0,0 +1,30 @@
+package com.yapp.timitimi.ui
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>(
+    @LayoutRes private val layoutId: Int
+) : AppCompatActivity() {
+
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+    }
+
+    fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        binding.unbind()
+        super.onDestroy()
+    }
+}
