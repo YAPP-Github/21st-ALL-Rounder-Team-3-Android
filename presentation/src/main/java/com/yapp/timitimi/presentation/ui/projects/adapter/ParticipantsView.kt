b54e74f62a7d0d088c1fc3f0d071a68913bdc7f0
EvergreenTree97 <tkdfhr7497@naver.com>
2023-02-12 23:01:57 +0900

    사용하지 않는 함수 주석처리

--- presentation/src/main/java/com/yapp/timitimi/presentation/ui/projects/adapter/ParticipantsView.kt
+++ presentation/src/main/java/com/yapp/timitimi/presentation/ui/projects/adapter/ParticipantsView.kt
@@ -1,9 +1,5 @@
 package com.yapp.timitimi.presentation.ui.projects.adapter
 
-import android.content.Context
-import android.util.AttributeSet
-import android.widget.LinearLayout
-
-class ParticipantsView : LinearLayout{
+/*class ParticipantsView : LinearLayout{
     
-}
+}*/
