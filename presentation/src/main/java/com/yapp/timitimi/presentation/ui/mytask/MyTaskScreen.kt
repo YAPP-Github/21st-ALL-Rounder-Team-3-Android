2a84cf753d2f5180d73748132518e95270793a1d
EvergreenTree97 <tkdfhr7497@naver.com>
2023-02-12 19:07:12 +0900

    bridge 이동하는 flow 생성

--- presentation/src/main/java/com/yapp/timitimi/presentation/ui/mytask/MyTaskScreen.kt
+++ presentation/src/main/java/com/yapp/timitimi/presentation/ui/mytask/MyTaskScreen.kt
@@ -8,31 +8,38 @@ import android.webkit.WebView
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.web.AccompanistWebChromeClient
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewNavigator
 import com.google.accompanist.web.rememberWebViewState
+import com.yapp.timitimi.presentation.ui.main.MainActivity
 import com.yapp.timitimi.presentation.ui.mytask.webview.MyTaskWebViewBridge
 import com.yapp.timitimi.presentation.ui.mytask.webview.MyTaskWebViewClient
+import com.yapp.timitimi.ui.startActivityWithAnimation
 
-const val Url = "http://10.0.2.2:8081/project/1/task/create"
+const val Url = "http://10.0.2.2:8081/project/1/task/1/edit"
 const val AccessTokenKey = "access_token" // TODO(EvergreenTree97) : 토큰 넣어야 함
+const val AccessTokenValue =
+    "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1NDRmMWY2Zi1hYzlhLTQ5NzMtYTFmMi02OTdkOGYwMGNiZTUiLCJyb2xlIjoiVVNFUiIsImV4cCI6MTY3NjI1Njg5Mn0.0HMJEXHuOBxON6neyEPtikKMvV4vMSNo_dGsyBfeNao"
 
 @Composable
 internal fun MyTaskScreen() {
     val activity = LocalContext.current as MyTaskActivity
+    var url by remember { mutableStateOf(Url) }
     val webViewClient = remember {
         MyTaskWebViewClient()
     }
     val webViewNavigator = rememberWebViewNavigator()
     val webViewState = rememberWebViewState(
-        url = Url,
+        url = url,
         additionalHttpHeaders = mapOf(),
     )
-    val accessTokenValue = ""
 
     BackHandler {
         if (webViewNavigator.canGoBack) {
@@ -45,7 +52,7 @@ internal fun MyTaskScreen() {
         CookieManager.getInstance().apply {
             setAcceptCookie(true)
             removeAllCookies(null)
-            setCookie(Url, "$AccessTokenKey=$accessTokenValue")
+            setCookie(Url, "$AccessTokenKey=$AccessTokenValue")
         }
     }
     WebView(
@@ -65,7 +72,14 @@ internal fun MyTaskScreen() {
                     useWideViewPort = true
                 }
                 addJavascriptInterface(
-                    MyTaskWebViewBridge(),
+                    MyTaskWebViewBridge(
+                        startMain = {
+                            activity.startActivityWithAnimation<MainActivity>()
+                        },
+                        updateUrl = {
+                            url = it
+                        }
+                    ),
                     MyTaskWebViewBridge.Name
                 )
             }
--- presentation/src/main/java/com/yapp/timitimi/presentation/ui/mytask/webview/Bridge.kt
+++ presentation/src/main/java/com/yapp/timitimi/presentation/ui/mytask/webview/Bridge.kt
@@ -1,13 +1,54 @@
 package com.yapp.timitimi.presentation.ui.mytask.webview
 
+import android.webkit.CookieManager
 import android.webkit.JavascriptInterface
+import com.yapp.timitimi.presentation.ui.mytask.AccessTokenKey
+import com.yapp.timitimi.presentation.ui.mytask.AccessTokenValue
+import com.yapp.timitimi.presentation.ui.mytask.Url
 import timber.log.Timber
 
-class MyTaskWebViewBridge() {
+const val BaseUrl = "http://10.0.2.2:8081"
+
+class MyTaskWebViewBridge(
+    private val startMain: () -> Unit,
+    private val updateUrl: (String) -> Unit,
+) {
 
     @JavascriptInterface
-    fun showToast(text: String) {
-        Timber.e(text)
+    fun navigateToMain() {
+        startMain()
+    }
+
+    @JavascriptInterface
+    fun navigateToEdit(
+        projectId: String,
+        taskId: String,
+    ) {
+        updateUrl("$BaseUrl/project/$projectId/task/$taskId")
+    }
+
+    @JavascriptInterface
+    fun navigateToFeedback(
+        projectId: String,
+        taskId: String,
+    ) {
+        updateUrl("$BaseUrl/project/$projectId/task/$taskId/feedback")
+    }
+
+    @JavascriptInterface
+    fun navigateToMyTask(
+        projectId: String,
+        taskId: String,
+    ) {
+        updateUrl("$BaseUrl/project/$projectId/task/my/$taskId")// /project/{{projectId}}/task/my/{{taskId}}
+    }
+
+    @JavascriptInterface
+    fun navigateToOtherTask(
+        projectId: String,
+        taskId: String,
+    ) {
+        updateUrl("$BaseUrl/project/$projectId/task/$taskId")
     }
 
     companion object {
