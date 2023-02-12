0aebeba429cfdbaa9b2eb72426f80aa3702df476
jsh-me <ppm_it@naver.com>
2023-02-12 20:22:04 +0900

    add domain entity

---
+++ domain/src/main/java/com/yapp/timitimi/domain/entity/Project.kt
@@ -0,0 +1,12 @@
+package com.yapp.timitimi.domain.entity
+
+data class Project(
+    val id: Int,
+    val name: String,
+    val startDate: String,
+    val dueDate: String,
+    val goal: String,
+    val projectStatus: String,
+    val progress: String,
+    val participantInfos: List<Participant>
+)
