70383128b0502a8d98bc9f5e92579629f683df6b
jsh-me <ppm_it@naver.com>
2023-02-21 23:32:27 +0900

    프로젝트 리스트 화면 viewmodel & network 통신 부분 추가

---
+++ presentation/src/main/java/com/yapp/timitimi/presentation/ui/projectlist/ProjectListActivity.kt
@@ -0,0 +1,45 @@
+package com.yapp.timitimi.presentation.ui.projectlist
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.yapp.timitimi.presentation.R
+import com.yapp.timitimi.presentation.databinding.ActivityProjectsBinding
+import com.yapp.timitimi.presentation.ui.projectlist.adapter.ProjectListAdapter
+import com.yapp.timitimi.ui.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.onEach
+
+@AndroidEntryPoint
+class ProjectListActivity: BaseActivity<ActivityProjectsBinding>(R.layout.activity_projects) {
+    private val viewModel: ProjectListViewModel by viewModels()
+
+    private val projectsAdapter: ProjectListAdapter by lazy {
+        ProjectListAdapter()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initViews()
+        observe()
+    }
+
+    private fun initViews() {
+        binding.projectRecyclerview.adapter = projectsAdapter
+    }
+
+    private fun observe() {
+        viewModel.uiState
+            .onEach { state ->
+                binding.state = state
+
+                when (state) {
+                    is ProjectListUiState.Succeed -> {
+                        projectsAdapter.submitList(state.list)
+                    }
+
+                    else -> Unit
+                }
+            }
+    }
+
+}
