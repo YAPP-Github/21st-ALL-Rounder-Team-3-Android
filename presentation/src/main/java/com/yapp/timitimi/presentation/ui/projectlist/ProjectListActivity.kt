4a00d9d0e8e48a02f12acd33567f8bce45205697
jsh-me <ppm_it@naver.com>
2023-02-21 23:35:45 +0900

    프로젝트 리스트 하단에 버튼 클릭리스너 추가

--- presentation/src/main/java/com/yapp/timitimi/presentation/ui/projectlist/ProjectListActivity.kt
+++ presentation/src/main/java/com/yapp/timitimi/presentation/ui/projectlist/ProjectListActivity.kt
@@ -1,9 +1,11 @@
 package com.yapp.timitimi.presentation.ui.projectlist
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.yapp.timitimi.presentation.R
 import com.yapp.timitimi.presentation.databinding.ActivityProjectsBinding
+import com.yapp.timitimi.presentation.ui.createproject.CreateProjectActivity
 import com.yapp.timitimi.presentation.ui.projectlist.adapter.ProjectListAdapter
 import com.yapp.timitimi.ui.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,6 +27,9 @@ class ProjectListActivity: BaseActivity<ActivityProjectsBinding>(R.layout.activi
 
     private fun initViews() {
         binding.projectRecyclerview.adapter = projectsAdapter
+        binding.projectCreateButton.setOnClickListener {
+            startActivity(Intent(this, CreateProjectActivity::class.java))
+        }
     }
 
     private fun observe() {
