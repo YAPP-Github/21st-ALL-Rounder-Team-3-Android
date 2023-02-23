0c19b00093ce5a1be4297d1f841ea024d7a0d968
EvergreenTree97 <tkdfhr7497@naver.com>
2023-02-23 23:31:09 +0900

    extras 추가

--- presentation/src/main/java/com/yapp/timitimi/presentation/constant/Extras.kt
+++ presentation/src/main/java/com/yapp/timitimi/presentation/constant/Extras.kt
@@ -2,4 +2,6 @@ package com.yapp.timitimi.presentation.constant
 
 object Extras{
     const val ProjectId = "ExtraProjectId"
+    const val TaskId = "ExtraTaskId"
+    const val IsMe = "ExtraIsMe"
 }
