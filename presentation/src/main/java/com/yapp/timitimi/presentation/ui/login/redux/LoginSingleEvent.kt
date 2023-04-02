c8fe1c850fec037839a7f98df1bb63214f2beff1
jsh-me <ppm_it@naver.com>
2023-04-02 20:43:41 +0900

    로그인 웹뷰 부분 삭제

--- presentation/src/main/java/com/yapp/timitimi/presentation/ui/login/redux/LoginSingleEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.yapp.timitimi.presentation.ui.login.redux
-
-import com.yapp.timitimi.redux.BaseSingleEvent
-
-sealed class LoginSingleEvent : BaseSingleEvent {
-    object NavigateToCreateProject: LoginSingleEvent()
-    object Finish: LoginSingleEvent()
-    data class ShowToast(val msg: String) : LoginSingleEvent()
-}
